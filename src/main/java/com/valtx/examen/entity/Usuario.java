1e27c275e2de1af5d468abdd85475823e8a58521
Ricardo Bueno <ricardo.bueno@osf.pe>
2021-08-20 10:59:46 -0500

    se cambia la conexion a postgres

--- src/main/java/com/valtx/examen/entity/Usuario.java
+++ src/main/java/com/valtx/examen/entity/Usuario.java
@@ -13,10 +13,10 @@ public class Usuario {
     @Column(name = "nombre", nullable = false)
     private String nombre;
 
-    @Column(name = "user", nullable = false)
+    @Column(name = "`user`", nullable = false)
     private String user;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "`password`", nullable = false)
     private String password;
 
     @OneToOne
